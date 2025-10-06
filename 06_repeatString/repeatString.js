0ee829bee50e0264758608faaf27f885300c4ce6
Sergio <93150267+sergio9-9@users.noreply.github.com>
2025-10-06 18:21:30 +0100

    Update the 06 exercise to contain solved exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    stringRepeated = '';
 
+    if (num < 0) {
+        stringRepeated = 'ERROR';
+    } else if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+        stringRepeated += string;
+        }
+    }
+
+    return stringRepeated;
 };
 
 // Do not edit below this line
