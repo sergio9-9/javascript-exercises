5bdd8b6f30ac9389e95d733fdc65406c8f6ded3a
Sergio <93150267+sergio9-9@users.noreply.github.com>
2025-10-07 12:45:16 +0100

    Partial solution of the 09 exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }    
+    }
+    else if (num2 < num1) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+
+    
 
 };
 
