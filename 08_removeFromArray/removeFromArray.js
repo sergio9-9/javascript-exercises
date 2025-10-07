b7771f6e1e991f6e43e9a62009f72a4aee27f5db
Sergio <93150267+sergio9-9@users.noreply.github.com>
2025-10-07 12:31:05 +0100

    Update the 08 exercise to include the solved exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    let index = 0;
+
+    // const args = Array.from(numbers);
+
+    numbers.forEach(number_element => {
+        array = array.filter(array_item => array_item !== number_element);
+
+        // index = array.indexOf(number_element);
+
+        // array.splice(index, 1);
+    });
+
+    return array;
 };
+
+// const remove = removeFromArray([1,2,3,4], 3, 2);
+// console.log(remove);
 
 // Do not edit below this line
 module.exports = removeFromArray;
