88c94287ac8ff0a61d3a7d27915a16091b0d6ce2
Sergio <93150267+sergio9-9@users.noreply.github.com>
2025-10-07 14:48:04 +0100

    Update the 09 exercise to contain the solved exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,20 +1,31 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    
-    if (num1 < num2) {
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (!(Number.isInteger(num1)) || !(Number.isInteger(num2))) {
+        return "ERROR";
+    }
+
+    else if (num1 < num2) {
         for (let i = num1; i <= num2; i++) {
             sum += i;
-        }    
+        }
+        return sum;    
     }
     else if (num2 < num1) {
         for (let i = num2; i <= num1; i++) {
             sum += i;
         }
-    }
-
+    } 
     
-
+    return sum;
 };
+
+
+console.log(sumAll(2.5, 4));
+
 
 // Do not edit below this line
 module.exports = sumAll;
